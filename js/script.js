ef599e63eed44ac09af88d9a576e302e0d0b1ca4
monsterindian <monsterindian@gmail.com>
2018-06-19 09:56:29 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,77 @@
+var $ = jQuery.noConflict();
+
+/*nav fixed top*/
+window.onscroll = function() {myFunction()};
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= 350) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+/*end nav fixed top*/
+
+/*page scroll*/
+$(document).ready(function(){
+  // Add scrollspy to <body>
+  $('body').scrollspy({target: ".navbar", offset: 50});   
+
+  // Add smooth scrolling on all links inside the navbar
+  $(".scroll a").on('click', function(event) {
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+   
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    }  // End if
+  });
+});
+
+/*End page scroll*/
+
+/*validation*/
+(function() {
+  'use strict';
+  window.addEventListener('load', function() {
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.getElementsByClassName('needs-validation');
+    // Loop over them and prevent submission
+    var validation = Array.prototype.filter.call(forms, function(form) {
+      form.addEventListener('submit', function(event) {
+        if (form.checkValidity() === false) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+        form.classList.add('was-validated');
+      }, false);
+    });
+  }, false);
+})();
+/*validation ends*/
+/*touch slider*/
+$(document).ready(function() {
+  
+$(".carousel").swiperight(function() {
+    $(this).carousel('prev');
+});
+$(".carousel").swipeleft(function() {  
+    $(this).carousel('next');
+});
+});
+    /*tuoch slider ends*/
---
+++ mail.php
@@ -0,0 +1,14 @@
+
+ 
+   <?php 
+$ToEmail = 'krishna@fintecconsulting.com'; 
+$EmailSubject = 'Site contact form'; 
+$mailheader = "From: ".$_POST["Email"]."\r\n"; 
+$mailheader .= "Reply-To: ".$_POST["Email"]."\r\n"; 
+$mailheader .= "Content-type: text/html; charset=iso-8859-1\r\n"; 
+$MESSAGE_BODY = "Name: ".$_POST["Name"]."\r\n"; 
+$MESSAGE_BODY .= "Email: ".$_POST["Email"]."\r\n"; 
+$MESSAGE_BODY .= "Message: ".nl2br($_POST["Message"])."\r\n"; 
+mail($ToEmail, $EmailSubject, $MESSAGE_BODY, $mailheader) or die ("Failure"); 
+header("Location: http://www.rocman.in/fintec/index.html");
+?>
